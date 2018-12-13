ce35a3ba13330008beae9128f8bfa781a8a262c9
NaveenPatancheru <patan.naveen@gmail.com>
2018-12-13 14:02:51 -0500

    deploy.sh change

--- deploy.sh
+++ deploy.sh
@@ -115,7 +115,7 @@ if [ -e "%DEPLOYMENT_SOURCE%\package.json" ]; then
 fi
 
 # 3. Angular Prod Build
-IF EXIST "%DEPLOYMENT_SOURCE%\angular.json" (
+IF EXIST "%DEPLOYMENT_SOURCE%/angular.json" (
 echo Building App in %DEPLOYMENT_SOURCE%…
 pushd "%DEPLOYMENT_SOURCE%"
 call :ExecuteCmd !NPM_CMD! run build
